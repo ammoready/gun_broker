1b8f2611086e639bc43713cf426df34c1e0f7b65
Dale Campbell <oshuma@gmail.com>
2015-01-31 06:43:21 -0500

    Forgot some specs.

--- spec/gun_broker/user_spec.rb
+++ spec/gun_broker/user_spec.rb
@@ -244,4 +244,31 @@ describe GunBroker::User do
     end
   end
 
+  context '#items_not_won' do
+    let(:endpoint) { [GunBroker::API::GUNBROKER_API, '/ItemsNotWon'].join }
+
+    context 'on success' do
+      it 'returns won Items' do
+        stub_request(:get, endpoint)
+          .with(headers: headers('X-AccessToken' => token))
+          .to_return(body: response_fixture('items'))
+
+        user = GunBroker::User.new(username, token: token)
+        expect(user.items_not_won).not_to be_empty
+        expect(user.items_not_won.first).to be_a(GunBroker::Item)
+      end
+    end
+
+    context 'on failure' do
+      it 'raises an exception' do
+        stub_request(:get, endpoint)
+          .with(headers: headers('X-AccessToken' => token))
+          .to_return(body: response_fixture('not_authorized'), status: 401)
+
+        user = GunBroker::User.new(username, token: token)
+        expect { user.items_not_won }.to raise_error(GunBroker::Error::NotAuthorized)
+      end
+    end
+  end
+
 end
